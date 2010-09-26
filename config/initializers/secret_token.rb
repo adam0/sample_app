b05d0bde30715bc1371a9c8f054b4ea053e814f1
Adam Pocsi <adam.pocsi@quaestorial.com>
2010-09-26 08:22:59 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7fdc5c53ae38e64dfaaa42eff789e85de757d8adeb2ac8a9bfbbcc0d7a6281988efbec0317769d510ba3420975d6d711af42ca39962680cdc3b7857f1ee11c17'
